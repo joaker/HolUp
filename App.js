a69117feb7030b6a55bc7a03a5533e1926fd20cf
John Oakley <jooakley@gmail.com>
2024-03-20 13:00:38 -0500

    hide react-navigation header (recipients header missing)

--- App.js
+++ App.js
@@ -9,7 +9,12 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Recipients" component={RecipientsScreen} />
       </Stack.Navigator>
